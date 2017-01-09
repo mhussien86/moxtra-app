18d36980015b8198d8ab99e403f4ad1c7d3d132c
mohamed <mohamed.hussein-khattab@vodafone.com>
2017-01-09 03:35:41 +0200

    Creating base activity

---
+++ app/src/main/java/com/madroid/moxtraapp/BaseActivity.java
@@ -0,0 +1,9 @@
+package com.madroid.moxtraapp;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by mohamed on 09/01/17.
+ */
+public class BaseActivity extends AppCompatActivity{
+}
