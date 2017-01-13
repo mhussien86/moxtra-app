d8e5f3ac2c38f609c04abb6de113f09097ffa2aa
mhussien86 <m.hussien.android@gmail.com>
2017-01-13 09:34:02 +0200

    adding some starter classes

--- app/src/main/java/com/madroid/moxtraapp/ui/MainActivity.java
+++ app/src/main/java/com/madroid/moxtraapp/ui/MainActivity.java
@@ -1,13 +1,19 @@
-package com.madroid.moxtraapp;
+package com.madroid.moxtraapp.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.madroid.moxtraapp.BaseActivity;
+import com.madroid.moxtraapp.R;
+
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+
 }
---
+++ app/src/main/java/com/madroid/moxtraapp/ui/SplashActivity.java
@@ -0,0 +1,7 @@
+package com.madroid.moxtraapp.ui;
+
+/**
+ * Created by mohamed on 13/01/17.
+ */
+public class SplashActivity {
+}
