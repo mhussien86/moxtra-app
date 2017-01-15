d69f5b2fb87cf9d24ebc52f10ea5c0fe0bd328d3
mhussien86 <m.hussien.android@gmail.com>
2017-01-15 15:12:37 +0200

    Creating login interactors

--- app/src/main/java/com/madroid/moxtraapp/interactors/LoginInteractor.java
+++ app/src/main/java/com/madroid/moxtraapp/interactors/LoginInteractor.java
@@ -1,7 +1,24 @@
 package com.madroid.moxtraapp.interactors;
 
+import com.madroid.moxtraapp.dtos.LoginRequestDTO;
+import com.madroid.moxtraapp.dtos.LoginResponseDTO;
+
 /**
  * Created by mohamed on 13/01/17.
  */
 public interface LoginInteractor {
+
+
+    void login(LoginRequestDTO loginRequestDTO, OnLoginFinished onLoginFinished);
+
+    interface OnLoginFinished{
+
+        void onLoginSucceed(LoginResponseDTO loginResponseDTO);
+        void onLoginFailed(String message);
+
+
+    }
+
+    void unsubscribe();
+
 }
--- app/src/main/java/com/madroid/moxtraapp/interactors/LoginInteractorImpl.java
+++ app/src/main/java/com/madroid/moxtraapp/interactors/LoginInteractorImpl.java
@@ -1,7 +1,23 @@
 package com.madroid.moxtraapp.interactors;
 
+import com.madroid.moxtraapp.dtos.LoginRequestDTO;
+
 /**
  * Created by mohamed on 13/01/17.
  */
-public class LoginInteractorImpl {
+public class LoginInteractorImpl implements LoginInteractor{
+
+
+
+
+
+    @Override
+    public void login(LoginRequestDTO loginRequestDTO, OnLoginFinished onLoginFinished) {
+
+    }
+
+    @Override
+    public void unsubscribe() {
+
+    }
 }
