288ae00881e55d45da4fd9c4f33f5e4254240de0
mhussien86 <m.hussien.android@gmail.com>
2017-01-31 19:14:29 +0200

    Adding moxtra sdk and initilize it

---
+++ app/src/main/java/com/madroid/moxtraapp/MoxieChatApplication.java
@@ -0,0 +1,25 @@
+package com.madroid.moxtraapp;
+
+import android.support.multidex.MultiDexApplication;
+import android.util.Log;
+
+import com.moxtra.sdk.MXAccountManager;
+import com.moxtra.sdk.MXSDKException;
+
+/**
+ * Created by mohamed on 31/01/17.
+ */
+public class MoxieChatApplication  extends MultiDexApplication {
+
+    private static final String TAG = "MoxieChatApplication";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        try {
+            MXAccountManager.createInstance(this, "oKGng7ZluXE", "gIjG16PZboo", true);
+        } catch (MXSDKException.InvalidParameter invalidParameter) {
+            Log.e(TAG, "Error when creating MXAccountManager instance.", invalidParameter);
+        }
+    }
+}
