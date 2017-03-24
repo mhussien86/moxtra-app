a01f014013245a5e6a9daa7439f2ea3e007338ec
mhussien86 <m.hussien.android@gmail.com>
2017-03-24 20:23:39 +0200

    Working on Categories

---
+++ app/src/main/java/com/madroid/moxtraapp/data/APIConstants.java
@@ -0,0 +1,14 @@
+package com.madroid.moxtraapp.data;
+
+/**
+ * Created by mohamed on 24/03/17.
+ */
+
+public class APIConstants {
+
+    public static final String API_PRODUCTION = "https://api.moxtra.com/v1/";
+    public static final String API_SANDBOX = "https://apisandbox.moxtra.com/v1/";
+
+
+
+}
---
+++ app/src/main/java/com/madroid/moxtraapp/data/api/CategoriesAPI.java
@@ -0,0 +1,11 @@
+package com.madroid.moxtraapp.data.api;
+
+/**
+ * Created by mohamed on 24/03/17.
+ */
+
+public interface CategoriesAPI {
+
+
+
+}
