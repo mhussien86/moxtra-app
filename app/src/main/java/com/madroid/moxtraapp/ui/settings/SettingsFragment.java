f149418c809ea30cebcb266cd5b18631a018f5d4
mhussien86 <m.hussien.android@gmail.com>
2017-03-25 18:51:15 +0200

    Adding settings design

--- app/src/main/java/com/madroid/moxtraapp/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/madroid/moxtraapp/ui/settings/SettingsFragment.java
@@ -7,11 +7,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.madroid.moxtraapp.BaseActivity;
 import com.madroid.moxtraapp.BaseFragment;
 import com.madroid.moxtraapp.R;
+import com.madroid.moxtraapp.dtos.LoginResponseDTO;
 import com.moxtra.sdk.MXAccountManager;
+
+import org.parceler.Parcels;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -27,11 +31,27 @@ public class SettingsFragment extends BaseFragment{
     @Bind(R.id.logout_button)
     Button logoutButton;
 
+    @Bind(R.id.contact_name)
+    TextView contactName ;
+
+    @Bind(R.id.contact_phone)
+    TextView contactPhone ;
+
+    @Bind(R.id.contact_email)
+    TextView contactEmail ;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.seetings_fragment,container,false);
         ButterKnife.bind(this,view);
+
+        LoginResponseDTO loginResponseDTO = Parcels.unwrap(getActivity().getIntent().getParcelableExtra("data"));
+
+        contactName.setText(loginResponseDTO.getResponse().getUserData().getFirstName()+loginResponseDTO.getResponse().getUserData().getLastName());
+        contactEmail.setText(loginResponseDTO.getResponse().getUserData().getEmail());
+        contactPhone.setText(loginResponseDTO.getResponse().getUserData().getPhone());
+
         return view;
     }
 
