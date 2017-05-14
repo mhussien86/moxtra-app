b67778f9744a19aa405d99e2b80d9d75b4188283
mhussien86 <m.hussien.android@gmail.com>
2017-05-14 13:18:08 +0200

    Creating container activity for meetings fragments

---
+++ app/src/main/java/com/madroid/moxtraapp/ui/meet/MeetingsContainerActivity.java
@@ -0,0 +1,46 @@
+package com.madroid.moxtraapp.ui.meet;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
+
+import com.madroid.moxtraapp.BaseActivity;
+import com.madroid.moxtraapp.R;
+import com.madroid.moxtraapp.ui.contactdetails.ContactDetailsFragment;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by mohamed on 14/05/17.
+ */
+
+public class MeetingsContainerActivity extends BaseActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.common_activity);
+        ButterKnife.bind(this);
+        startJoinMeetFragment();
+
+    }
+
+    private void startContactsDetailsFragment() {
+
+        ContactDetailsFragment contactDetailsFragment = new ContactDetailsFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.layCommonActivity, contactDetailsFragment).commit();
+    }
+
+    private void startJoinMeetFragment() {
+
+        JoinMeetingFragment joinMeetingFragment = new JoinMeetingFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.layCommonActivity, joinMeetingFragment).commit();
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ButterKnife.unbind(this);
+    }
+}
