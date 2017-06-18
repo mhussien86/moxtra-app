7797adc3bb83c7e5a9ea286895b7c0f5e7cd1580
Omar Makeen <omar.makeen@vodafone.com>
2017-06-18 03:22:27 +0200

    DEVELOP: categories page

--- app/src/main/java/com/madroid/moxtraapp/ui/categories/RecentPeopleAdapter.java
+++ app/src/main/java/com/madroid/moxtraapp/ui/categories/RecentPeopleAdapter.java
@@ -2,14 +2,19 @@ package com.madroid.moxtraapp.ui.categories;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.amulyakhare.textdrawable.TextDrawable;
 import com.madroid.moxtraapp.R;
 import com.madroid.moxtraapp.dtos.binders.BindersResponseDTO;
+import com.moxtra.binder.sdk.MXException;
+import com.moxtra.sdk.MXChatManager;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
@@ -20,11 +25,13 @@ import de.hdodenhof.circleimageview.CircleImageView;
  * Created by mohamed on 25/03/17.
  */
 
-public class RecentPeopleAdapter extends RecyclerView.Adapter<RecentPeopleAdapter.MyViewHolder>{
+public class RecentPeopleAdapter extends RecyclerView.Adapter<RecentPeopleAdapter.MyViewHolder> implements MXChatManager.OnOpenChatListener{
 
-
+    private static final String TAG = "OpenBinder";
     private final List<BindersResponseDTO.Binder> recentPeople;
     private final Context mContext;
+    MXChatManager.OnOpenChatListener onOpenChatListener;
+
     public RecentPeopleAdapter(List<BindersResponseDTO.Binder> recentPeople, Context context) {
         this.recentPeople = recentPeople;
         this.mContext = context;
@@ -41,7 +48,7 @@ public class RecentPeopleAdapter extends RecyclerView.Adapter<RecentPeopleAdapte
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
 
-        BindersResponseDTO.Binder binder = recentPeople.get(position);
+        final BindersResponseDTO.Binder binder = recentPeople.get(position);
 //        try {
 //            URL url = new URL(binder.getSubBinder().getThumbnailUri());
 //            Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
@@ -52,22 +59,25 @@ public class RecentPeopleAdapter extends RecyclerView.Adapter<RecentPeopleAdapte
 //
 //        }
 
-//        Picasso.with(mContext).load(binder.getSubBinder().getThumbnailUri()).into(holder.profileImage);
-//        TextDrawable drawable = TextDrawable.builder()
-//                .buildRound("A", ResourcesCompat.getColor(mContext.getResources(), R.color.oldlavender, null));
-
-
         if(hasPersonalImage(binder.getSubBinder().getThumbnailUri())){
             Picasso.with(mContext).load(binder.getSubBinder().getThumbnailUri()).into(holder.profileImage);
             holder.profileImage.setVisibility(View.VISIBLE);
-//            holder.profileText.setVisibility(View.GONE);
+            holder.profileImage.setOnClickListener(new Button.OnClickListener() {
+                public void onClick(View v) {
+                    openChat(binder.getSubBinder().getId());
+                }
+            });
         }
         else {
             TextDrawable drawable = TextDrawable.builder().beginConfig().fontSize(35).endConfig()
                     .buildRound(initials(binder.getSubBinder().getName()), mContext.getResources().getColor(R.color.light_gray));
             holder.profileText.setImageDrawable(drawable);
-//            holder.profileImage.setVisibility(View.GONE);
             holder.profileText.setVisibility(View.VISIBLE);
+            holder.profileText.setOnClickListener(new Button.OnClickListener() {
+                public void onClick(View v) {
+                    openChat(binder.getSubBinder().getId());
+                }
+            });
         }
     }
 
@@ -107,4 +117,26 @@ public class RecentPeopleAdapter extends RecyclerView.Adapter<RecentPeopleAdapte
         }
         return initials.toString().toUpperCase();
     }
+
+    public void openChat(String binderId){
+        try{
+            MXChatManager.getInstance().openChat(binderId, onOpenChatListener);
+        }
+        catch (MXException.AccountManagerIsNotValid accountManagerIsNotValid){
+            Log.e(TAG, "Error when creating MXAccountManager instance.", accountManagerIsNotValid);
+        }
+
+    }
+
+    @Override
+    public void onOpenChatSuccess() {
+        Log.i(TAG, "Open chat success.");
+    }
+
+    @Override
+    public void onOpenChatFailed(int i, String s) {
+        Log.e(TAG, "Failed to open chat with code: " + i + ", msg: " + s);
+        Toast.makeText(mContext, "Failed to open chat: " + s, Toast.LENGTH_LONG).show();
+    }
 }
+
